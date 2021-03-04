fc9c9fcbf09d72fcc2c32b9fec2ccdb0f3ec33c6
Mateus <mateusmuller2@gmail.com>
2021-03-04 09:29:57 -0300

    Master (#1)
    * first functions to grab reviews and courses, still testing de api
    
    * adding features to grab reviews and save on file to compare on the next day
    
    * adding diff and show functions, rewriting pull function to populate json db
    
    * removing db
    
    * adding run function
    
    * trying to rebase
    
    * adding run function
    
    * adding new files

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from udemy.instructor import UdemyInstructor
+import os
+
+if __name__ == '__main__':
+  api_key = os.getenv('UDEMY_API_KEY')
+
+  udemy_instructor = UdemyInstructor(api_key)
+  udemy_instructor.run()
