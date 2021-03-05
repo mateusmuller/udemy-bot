22507738175256121241c74bb361b0eb01f033be
Mateus <mateusmuller2@gmail.com>
2021-03-05 09:58:24 -0300

    modifying db path to db folder

--- udemy/reviews.py
+++ udemy/reviews.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import base64
 from os import path
 from udemy.decorators import Decorators
 
@@ -23,11 +22,11 @@ class UdemyInstructor:
 
   @staticmethod
   def create_db_file():
-    if not path.exists("old_reviews.txt"):
-      with open("old_reviews.txt", "w") as file:
+    if not path.exists("db/old_reviews.txt"):
+      with open("db/old_reviews.txt", "w") as file:
         file.write("")
-    if not path.exists("new_reviews.txt"):
-      with open("new_reviews.txt", "w") as file:
+    if not path.exists("db/new_reviews.txt"):
+      with open("db/new_reviews.txt", "w") as file:
         file.write("")
 
   def pull_new_reviews(self):
@@ -36,28 +35,28 @@ class UdemyInstructor:
       headers=self.authorization
     )
 
-    with open ("db.json", "w") as db:
+    with open ("db/db.json", "w") as db:
       json.dump(response.json(), db, indent=2)
 
     reviews = json.loads(response.content)
     course_ids = '\n'.join(reviews["id"] for reviews in reviews["results"])
 
-    with open("new_reviews.txt", "w") as file:
+    with open("db/new_reviews.txt", "w") as file:
           file.write(course_ids)
 
   def get_diff_reviews(self):
-    with open("old_reviews.txt") as old_reviews:
-      with open("new_reviews.txt") as new_reviews:
+    with open("db/old_reviews.txt") as old_reviews:
+      with open("db/new_reviews.txt") as new_reviews:
         strip_diff = [diff.strip() for diff in set(new_reviews).difference(old_reviews)]
     return strip_diff
 
   def set_old_db(self):
-    with open ("new_reviews.txt", "r") as new_reviews:
-      with open ("old_reviews.txt", "w") as old_reviews:
+    with open ("db/new_reviews.txt", "r") as new_reviews:
+      with open ("db/old_reviews.txt", "w") as old_reviews:
         old_reviews.write(new_reviews.read())
 
   def show_new_reviews(self):
-    with open ("db.json", "r") as file:
+    with open ("db/db.json", "r") as file:
 
       messages = []
       reviews = json.load(file)
@@ -75,12 +74,6 @@ class UdemyInstructor:
       requests.post(self.webhook_url, data=payload)
 
     self.set_old_db()
-
-  # def decrypt_course_id(self):
-  #     with open ("new_reviews.txt", "r", encoding="utf-8") as new_reviews:
-  #       for i in new_reviews.readlines():
-  #         message = base64.b64decode(i)
-  #         print(message)
 
   def run(self):
     self.create_db_file()
